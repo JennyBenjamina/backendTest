0a213aa2f334622aacaf8a29d85b54c613583560
Jenny <jennylee1989@gmail.com>
2024-05-22 08:45:20 -0700

    route added

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/api", (req, res) => {
+  res.send("api route");
+});
+
 app.listen(port, () => {
   console.log(`Server is running ${port}`);
 });
