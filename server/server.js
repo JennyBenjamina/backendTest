e137b5678ad16e4358cafff6382f676e94af3e9c
Jenny <jennylee1989@gmail.com>
2024-05-22 08:20:19 -0700

    dotenv

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.use(
